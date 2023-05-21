ebb6c378bacf38d7017416c7bd75bd335e159ba2
FHenric <ffilipehhenrique@gmail.com>
2023-05-21 08:16:45 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 app.get('/jujuba', () => {
   return 'Oi meu amorzinho <3'
-});
+})
 
-app.listen({
-  port:3333,
-}).then(() => {
-  console.log("Dale bença, tá rodando já visse: http://localhost:3333")
-});
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Dale bença, tá rodando já visse: http://localhost:3333')
+  })
